74eb77b5b280ebd616326318cf177a9521b4b5c6
Sayan Chatterjee <sayanchatt5584@gmail.com>
2024-05-24 02:32:05 +0530

    Added Play Again Button

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -6,11 +6,17 @@ function GameOver() {
   const { gameOver, currAttempt, correctWord } = useContext(AppContext);
   const winner = gameOver.guessedWord;
   const winStyle = winner ? { color: "green" } : { color: "red" };
+  function refreshPage() {
+    window.location.reload();
+  }
   return (
     <div className="gameOver">
       <h3 style={winStyle}>{winner ? "You Correctly Guessed the Word" : "You Failed to Guess the Word"}</h3>
       <h2>Correct Word: {correctWord.toUpperCase()}</h2>
       {gameOver.guessedWord && <h3>You guessed in {currAttempt.attempt} attempts</h3>}
+      <button className="reloadBtn" onClick={refreshPage}>
+        <span>Play Again! ⟳</span>
+      </button>
     </div>
   );
 }
